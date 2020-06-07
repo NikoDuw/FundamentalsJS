1129d94dbbc98e7b6712045a422659d6d6fd3392
Niko_Duw <bndm1410@gmail.com>
2020-06-07 15:31:59 -0500

    Clase3

---
+++ clase3.js
@@ -0,0 +1,37 @@
+var edad = 27
+
+//edad = edad + 1
+
+edad += 1 
+
+var peso = 75
+
+//peso = peso -2
+
+peso -= 2
+
+var sandwich = 1
+
+peso = peso + sandwich
+
+
+var playSoccer = 3
+
+//peso = peso - playSoccer
+
+peso -= playSoccer
+
+var precioVino = 200.3
+
+var total = Math.round(precioVino * 100 * 3)/100
+
+var totalStr = total.toFixed(2)
+
+var total2 = parseFloat(totalStr)
+
+
+var pizza = 8
+var personas = 2
+
+var cantidadDeProcionesPorPersona = pizza/personas
+
