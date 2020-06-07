0fe728a28c27e9c4ebd35a7707dfc8486fa614f3
Niko_Duw <bndm1410@gmail.com>
2020-06-07 17:17:27 -0500

    Clase4

---
+++ clase4.js
@@ -0,0 +1,10 @@
+var nombre ='Nicolás', edad = 27
+
+function imprimirEdad(n, e)
+{
+    console.log(`${n} tiene ${e} años`);
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Nick', 28)
+imprimirEdad('Beny', 27)
